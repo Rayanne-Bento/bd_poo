25c47ea35983b2f425e85376d50c0f64ee6cc759
Rayanne <rayanne.bento@academico.ifpb.edu.br>
2021-03-07 12:11:36 -0300

    Sequeliza

---
+++ src/model/Cliente.js
@@ -0,0 +1,19 @@
+let {Sequelize, DataTypes } = require('sequelize');
+let db = require('../db');
+
+const Cliente = db.define('Cliente',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        telefone: DataTypes.STRING
+    }
+);
+module.exports = Cliente;
